e6c3a9407ecdf5d5748b102e5408b9862d81b2f6
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2022-03-07 22:40:59 +0900

    dp_c

--- dp_c/src/main.rs
+++ dp_c/src/main.rs
@@ -6,49 +6,17 @@ fn main() {
         z: [(u64, u64, u64);n],
     }
 
-    let mut dp = vec![0_u64; n];
-    let mut last = -1;
-    let (a, b, c) = z[0];
-    if a >= b && a >= c {
-        dp[0] = a;
-        last = 0;
-    } else if b >= a && b >= c {
-        dp[0] = b;
-        last = 1;
-    } else if c >= b && c >= a {
-        dp[0] = c;
-        last = 2;
+    let mut dp = vec![vec![0_u64; 3]; n];
+    dp[0][0] = z[0].0;
+    dp[0][1] = z[0].1;
+    dp[0][2] = z[0].2;
+    for i in 1..n {
+        dp[i][0] = std::cmp::max(dp[i - 1][1], dp[i - 1][2]) + z[i].0;
+        dp[i][1] = std::cmp::max(dp[i - 1][0], dp[i - 1][2]) + z[i].1;
+        dp[i][2] = std::cmp::max(dp[i - 1][0], dp[i - 1][1]) + z[i].2;
     }
-    for (i, (a, b, c)) in z.iter().enumerate() {
-        if i == 0 {
-            continue;
-        }
-
-        if last == 0 {
-            if *b > *c {
-                dp[i] = dp[i - 1] + *b;
-                last = 1;
-            } else {
-                dp[i] = dp[i - 1] + *c;
-                last = 2;
-            }
-        } else if last == 1 {
-            if *a > *c {
-                dp[i] = dp[i - 1] + *a;
-                last = 0;
-            } else {
-                dp[i] = dp[i - 1] + *c;
-                last = 2;
-            }
-        } else if last == 2 {
-            if *a > *b {
-                dp[i] = dp[i - 1] + *a;
-                last = 0;
-            } else {
-                dp[i] = dp[i - 1] + *b;
-                last = 1;
-            }
-        }
-    }
-    println!("{}", dp[n - 1]);
+    println!(
+        "{}",
+        std::cmp::max(std::cmp::max(dp[n - 1][0], dp[n - 1][1]), dp[n - 1][2])
+    );
 }
