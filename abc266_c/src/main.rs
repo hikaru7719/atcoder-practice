ec5d54695cbd668381bbc67b45dae7871399606b
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2022-09-01 09:18:09 +0900

    abc266 c

--- abc266_c/src/main.rs
+++ abc266_c/src/main.rs
@@ -8,34 +8,33 @@ fn main() {
     d: (i32, i32),
     };
 
-    if check(a, c, b, d) {
+    if cross(vec(b, a), vec(d, a)) < 0 {
         println!("No");
         return;
     }
-    if check(b, d, a, c) {
+
+    if cross(vec(c, b), vec(a, b)) < 0 {
         println!("No");
         return;
     }
+
+    if cross(vec(d, c), vec(b, c)) < 0 {
+        println!("No");
+        return;
+    }
+
+    if cross(vec(a, d), vec(c, d)) < 0 {
+        println!("No");
+        return;
+    }
+
     println!("Yes");
 }
 
-fn check(a: (i32, i32), c: (i32, i32), b: (i32, i32), d: (i32, i32)) -> bool {
-    let x1: i32 = std::cmp::max(b.0, d.0);
-    let x2: i32 = std::cmp::min(b.0, d.0);
-    let y1: i32 = std::cmp::max(b.1, d.1);
-    let y2: i32 = std::cmp::min(b.1, d.1);
-    if a.0 < x2 && c.0 < x2 {
-        return true;
-    }
-    if x1 < a.0 && x1 < c.0 {
-        return true;
-    }
-    if a.1 < y2 && c.1 < y2 {
-        return true;
-    }
+fn cross(x: (i32, i32), y: (i32, i32)) -> i32 {
+    x.0 * y.1 - x.1 * y.0
+}
 
-    if y1 < a.1 && y1 < c.1 {
-        return true;
-    }
-    false
+fn vec(x: (i32, i32), y: (i32, i32)) -> (i32, i32) {
+    (x.0 - y.0, x.1 - y.1)
 }
