1930718ead091df642d5153bf92753f6e2cb8303
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2025-04-29 22:09:46 +0900

    abc401 d

--- abc401_d/main.py
+++ abc401_d/main.py
@@ -1,18 +1,44 @@
 n, k = map(int, input().split())
+t = list(input())
 
-a = [0 for _ in range(n + 1)]
-s = [0 for _ in range(n + 1)]
-a[0] = 1
-s[0] = 1
+for i in range(n):
+    if t[i] == "?":
+        if (0 < i and t[i - 1] == "o") or (i < n - 1 and t[i + 1] == "o"):
+            t[i] = "."
 
-for i in range(1, n + 1):
-    if i < k:
-        a[i] = 1 % (10**9)
-    elif i == k:
-        a[i] = s[i - 1] % (10**9)
+    if t[i] == "o":
+        k -= 1
+
+if k == 0:
+    for i in range(n):
+        if t[i] == "?":
+            t[i] = "."
+
+
+index = 0
+segment = []
+while index < n:
+    if t[index] == "?":
+        start = index
+        length = 0
+        while index < n and t[index] == "?":
+            length += 1
+            index += 1
+        segment.append((start, length))
     else:
-        a[i] = (s[i - 1] - s[i - k - 1]) % (10**9)
-    s[i] = (s[i - 1] + a[i]) % (10**9)
+        index += 1
 
+k_max = 0
+for start, length in segment:
+    if length % 2 == 0:
+        k_max += length // 2
+    else:
+        k_max += (length + 1) // 2
 
-print(a[n])
+if k_max == k:
+    for start, length in segment:
+        if length % 2 == 1:
+            for j in range(length):
+                t[start + j] = "o" if j % 2 == 0 else "."
+
+print("".join(t))
